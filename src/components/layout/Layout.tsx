66ef0726b551903fcf6549291acc80184a98eaca
priyanshu lohar <priyanshulohar1@gmail.com>
2025-03-27 18:30:23 +0530

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -43,7 +43,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 
   return (
     <div className="flex flex-col min-h-screen">
-      <Marquee />
+      {/*<Marquee />*/}
       <Navbar />
       <main className="flex-grow pt-20">
         {children}
